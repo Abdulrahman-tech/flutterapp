c73bf88aeed7f5d5136d55236be4e897d8841266
Abdulrahman-tech <abdullateefa01@gmail.com>
2022-08-19 23:12:21 +0100

    my first commit

---
+++ lib/widgets/icon_widget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+import '../utils/app_layout.dart';
+import '../utils/app_styles.dart';
+
+class AppIconText extends StatelessWidget {
+  final IconData icons;
+  final String text;
+  const AppIconText({Key? key, required this.icons, required this.text})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        vertical: AppLayout.getWidth(12),
+        horizontal: AppLayout.getWidth(12),
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(
+          AppLayout.getWidth(10),
+        ),
+      ),
+      child: Row(
+        children: [
+          Icon(
+            icons,
+            color: const Color(0xFFBFC2DF),
+          ),
+          Gap(
+            AppLayout.getHeight(10),
+          ),
+          Text(
+            'Departure',
+            style: Styles.textStyle,
+          ),
+        ],
+      ),
+    );
+  }
+}
